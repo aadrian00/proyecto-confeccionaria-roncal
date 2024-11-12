ca963c23e1ef13332e1ecd40372ff002e98dbfaf
Arnodorian020 <97480853+Arnodorian020@users.noreply.github.com>
2024-11-11 21:52:57 -0500

    Corrigiendo App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainNav from './components/MainNav';
+import MainNav from './Components/MainNav';
 import Login from './Login/login';
 import Inicio from './Pages/inicio';
 import Reporte from './Pages/Reporte';
