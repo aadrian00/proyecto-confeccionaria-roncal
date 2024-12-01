c031501e20a82b42e768d7dabff8e937fce335ab
Ultrafer50 <ultrafer50@gmail.com>
2024-12-01 14:06:59 -0500

    Arreglar import

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,13 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainNav from './Components/MainNav';
+import MainNav from './components/MainNav';
 import Login from './Login/login';
 import Inicio from './Pages/inicio';
 import Reporte from './Pages/Reporte';
 import CrearInsumosPage from './Pages/PaginaCrearInsumos';
 import EditarInsumoPage from './Pages/PaginaEditarInsumos';
 import Historico from './Pages/Historico';
+import Notificaciones from './Pages/Notificaciones';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/historico" element={<Historico />} />
         <Route path="/crearInsumo" element={<CrearInsumosPage />}/>
         <Route path="/editar-insumo/:id" element={<EditarInsumoPage />} />
+        <Route path="/editar-notificaciones" element={<Notificaciones />} />
       </Routes>
     </Router>
   );
